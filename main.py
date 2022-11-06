cc24a4dca312dd1b06a75b856ccf97c4148fb528
khomi-a <khomutov.aa@phystech.edu>
2022-11-06 16:49:47 +0800

    added data

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+def construct_dB_grap():
+    pass
+
+
+def save_contigs():
+    pass
+
+
+def invert_sequence(s):
+    """
+    Makes complimentary inversion of the string
+    :param s: input sequence
+    :return: inverted sequence
+    """
+    d_inv = {'A': 'T', 'G': 'C', 'T': 'A', 'C': 'G'}
+    return ''.join([d_inv[s[i]] for i in range(len(s) - 1, 0, -1)])
 
 
 if __name__ == "__main__":
-    pass
-
+    construct_dB_grap()
+    save_contigs()

ac1bd5bbf7c414913579b009be80275dcc43fb4c
khomi-a <khomutov.aa@phystech.edu>
2022-11-07 21:54:46 +0800

    attributes added, update function renamed

--- de_Bruijn_graph.py
+++ de_Bruijn_graph.py
@@ -1,5 +1,7 @@
 import networkx as nx
-import matplotlib.pyplot as plt
+from tqdm import tqdm
+# import matplotlib.pyplot as plt
+
 
 class Kmer():
     def __init__(self, s):
@@ -10,33 +12,43 @@ class Kmer():
 
 
 class De_Bruijn_graph():
-    def __int__(self, k=10):
-        self.G = nx.Graph()
+    def __init__(self, k=10):
+        self.G = nx.MultiDiGraph()
         self.k = k
+        self.components = []
+        self.num_components = 0
 
     def get_kmers(self, seq):
-        return [Kmer(seq[i:i+self.k]) for i in range(len(seq) - self.k)]
+        return [Kmer(seq[i:i + self.k]) for i in range(len(seq) - self.k + 1)]
 
+    def update_graph(self, fq):
+        for i in tqdm(range(len(fq))):
+            for kmer in self.get_kmers(fq[i].seq):
+                self.G.add_nodes_from([kmer.suffix, kmer.prefix])
+                self.G.add_edge(kmer.suffix, kmer.prefix, label=kmer.kmer)
+        self.get_components()
 
-    def build_graph(self, fq):
-        for read in fq:
-            for kmer in self.get_kmers(read.seq):
-                self.G.add_nodes_from([kmer.suffix, kmer.prefix)
+    def get_components(self):
+        comp_nodes = nx.weakly_connected_components(self.G)
+        self.components = [self.G.subgraph(nodes) for nodes in comp_nodes]
+        self.num_components = len(self.components)
 
-    def build_graph_from_strings(self, s):
-        for read in s:
-            for kmer in self.get_kmers(read):
-                self.G.add_nodes_from([kmer.suffix, kmer.prefix)
+    @staticmethod
+    def make_contig_of_path(path):
+        contig = ''
+        for u, v in path:
+            if len(contig) == 0:
+                contig += u + v[-1]
+            else:
+                contig += v[-1]
+        return contig
 
-def visualize_graph(graph):
-    """
-    A naive method to plot graph
-    :param G:
-    :return:
-    """
-    nx.draw(graph, with_labels=True, cmap=plt.get_cmap('jet'))
-    plt.show()
-
-
+    def get_contigs(self):
+        contigs = []
+        for component in tqdm(self.components):
+            if nx.has_eulerian_path(component):
+                contig = self.make_contig_of_path(nx.eulerian_path(component))
+                contigs.append(contig)
+        return contigs
 
 
